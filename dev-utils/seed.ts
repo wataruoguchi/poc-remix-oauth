a1c617fd4d6005ddc8bd2162cb6d2e101c7f34ee
wataru <wataru.oguchi.0to1@gmail.com>
2024-07-18 22:17:56 -0700

    [#tool] seed-d1

--- dev-utils/seed.ts
+++ dev-utils/seed.ts
@@ -1,14 +1,28 @@
 import { drizzle } from "drizzle-orm/d1";
+import { execSync } from "node:child_process";
+import { seeder } from "seed-d1";
 import { insertUserSchema, users } from "../app/schema";
 import { mockUsers as _mockUsers } from "./mockUsers";
-import { seeder } from "./seeder";
 
-seeder("poc-remix-oauth", "./dev-utils/seed.local.sql", [
-  (d1: D1Database) => drizzle(d1).delete(users).toSQL(),
-  (d1: D1Database) => {
-    const mockUsers = _mockUsers.map((mockUser) =>
-      insertUserSchema.parse(mockUser)
-    );
-    return drizzle(d1).insert(users).values(mockUsers).toSQL();
-  },
-]);
+const databaseName = "poc-remix-oauth";
+const sqlDistPath = "./dev-utils/seed.local.sql";
+
+seeder(
+  sqlDistPath,
+  [
+    (d1: D1Database) => drizzle(d1).delete(users).toSQL(),
+    (d1: D1Database) => {
+      const mockUsers = _mockUsers.map((mockUser) =>
+        insertUserSchema.parse(mockUser)
+      );
+      return drizzle(d1).insert(users).values(mockUsers).toSQL();
+    },
+  ],
+  async () => {
+    const command = `npx wrangler d1 execute ${databaseName} --file="${sqlDistPath}"`;
+    console.log(command);
+    execSync(command, {
+      encoding: "utf-8",
+    });
+  }
+);
